e6f4fa9190a3b7ba0be76608fa7340ad3e8b1825
a-y-m-a-n-c-h <mmc50@mail.aub.edu>
2024-07-02 13:52:33 +0200

    added bin folders, fixed some minor errors in puttoken and gettoken

--- src/serial/Framework/EDGetToken.jl
+++ src/serial/Framework/EDGetToken.jl
@@ -2,10 +2,25 @@ module edm
 
 const UNINITIALIZED_VALUE = UInt32(0xFFFFFFFF)
 
+
+struct EDGetTokenT{T}
+    value::UInt32
+    EDGetTokenT() = new{T}(UNINITIALIZED_VALUE)
+end
+
+function index(token::EDGetTokenT{T}) where T 
+    return token.value
+end
+
+function isUninitialized(token::EDGetTokenT{T}) where T
+    return token.value == UNINITIALIZED_VALUE
+end
+
+
 struct EDGetToken
     value::UInt32
     EDGetToken() = new(UNINITIALIZED_VALUE)
-    function EDGetToken{T}(token::EDGetTokenT{T})
+    function EDGetToken(token::EDGetTokenT{T}) where T
         new(token.value)
     end
 end
@@ -18,17 +33,5 @@ function isUninitialized(token::EDGetToken)
     return token.value == UNINITIALIZED_VALUE
 end
 
-struct EDGetTokenT{T}
-    value::UInt32
-    EDGetTokenT() = new(UNINITIALIZED_VALUE)
-end
-
-function index(token::EDGetTokenT{T})
-    return token.value
-end
-
-function isUninitialized(token::EDGetTokenT{T})
-    return token.value == UNINITIALIZED_VALUE
-end
 
 end 
