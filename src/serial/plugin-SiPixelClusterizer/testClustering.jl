a9ed3e83f917007f81e0c0c00f3ae330b4f18dc6
Maya Ali <maa351@mail.aub.edu>
2024-07-09 16:36:46 +0200

    debug 4

---
+++ src/serial/plugin-SiPixelClusterizer/testClustering.jl
@@ -0,0 +1,38 @@
+module ClusteringTest
+
+using Printf
+using Test
+
+include("../plugin-SiPixelClusterizer/gpu_clustering.jl")
+using .gpuClustering:find_clus, count_modules
+
+const INV_ID = 65535
+const MAX_NUM_MODULES = 10
+const num_cols_in_module = 416
+
+# Mockup data for testing
+id = UInt16[1, 1, 1, 1, 1, 1, 1, 1]
+x = UInt16[1, 2, 3, 1, 2, 3, 1, 2]
+y = UInt16[1, 1, 1, 2, 2, 2, 3, 3]
+
+num_elements = length(id)
+module_start = fill(1, MAX_NUM_MODULES + 1)
+module_start[1] = 1
+cluster_id = fill(1, num_elements)
+n_clusters_in_module = fill(UInt32(0), MAX_NUM_MODULES + 1)
+moduleId = fill(1, MAX_NUM_MODULES + 1)
+
+expected_clusters = 1
+
+count_modules(id, module_start, cluster_id, num_elements)
+
+find_clus(id, x, y, module_start, n_clusters_in_module, moduleId, cluster_id, num_elements)
+
+actual_clusters = n_clusters_in_module[1]
+
+println("Expected number of clusters: ", expected_clusters)
+println("Actual number of clusters: ", actual_clusters)
+
+@test actual_clusters == expected_clusters
+
+end
