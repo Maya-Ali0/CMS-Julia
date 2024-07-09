44ce3711dff7c8d0a2a5d9a947e7db338e15a9da
a-y-m-a-n-c-h <mmc50@mail.aub.edu>
2024-07-10 01:48:42 +0200

    translated ESProducers SiPixelFedCablingMapGPUWrapperESProducer and SiPixelGainCalibrationForHLTGPUESProducer to julia, and added workflow of main

--- src/serial/bin/main.jl
+++ src/serial/bin/main.jl
@@ -4,12 +4,24 @@ using .dataFormats
 include("ReadRaw.jl")
 include("../Framework/EandES.jl")
 
+include("../plugin-SiPixelClusterizer/SiPixelFedCablingMapGPUWrapperESProducer.jl")
+include("../plugin-SiPixelClusterizer/SiPixelGainCalibrationForHLTGPUESProducer.jl")
 
 raw_events = readall(open((@__DIR__) * "/../../../data/raw.bin")) # Reads 1000 events
 
 es::EventSetup = EventSetup()
 
+dataDir::String = (@__DIR__) * "/../../../data/raw.bin"
+
+cabling_map_producer::SiPixelFedCablingMapGPUWrapperESProducer = SiPixelFedCablingMapGPUWrapperESProducer(dataDir)
+gain_Calibration_producer::SiPixelGainCalibrationForHLTGPUESProducer = SiPixelGainCalibrationForHLTGPUESProducer(dataDir)
+
+produce(cabling_map_producer,es)
+produce(gain_Calibration_producer,es)
+
 for event ∈ raw_events
-    
+
 end
+
+
 
