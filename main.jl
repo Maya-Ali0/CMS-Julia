bb2fa8747c4cfe2ca4ec0358c6b03fe406b09095
Maya Ali <maa351@mail.aub.edu>
2025-03-14 01:17:11 +0200

    Removed Profile library include

--- main.jl
+++ main.jl
@@ -1,7 +1,8 @@
 # using Revise
 include("src/Patatrack.jl")
 using .Patatrack
-using Profile, BenchmarkTools, ProfileView
+# using Profile, ProfileView
+using BenchmarkTools
 const num_of_threads::Int = 1
 const num_of_streams::Int = 0
 const warm_up_events::Int = 0 # Number of events to process before starting the benchmark (default 0).
@@ -13,33 +14,33 @@ const empty::Bool = false # Ignore all producers (used for testing only)
 
 function run(num_of_streams::Int)
 
-ed_modules::Vector{String} = String[]
-es_modules::Vector{String} = String[]
+    ed_modules::Vector{String} = String[]
+    es_modules::Vector{String} = String[]
 
-if(!empty)
-    ed_modules = ["SiPixelRawToClusterCUDA","BeamSpotToPOD", "SiPixelRecHitCUDA", "CAHitNtupletCUDA", "PixelVertexProducerCUDA"]
-    es_modules = ["SiPixelFedCablingMapGPUWrapperESProducer","SiPixelGainCalibrationForHLTGPUESProducer","PixelCPEFastESProducer","BeamSpotESProducer"]
-end
-if(validation)
-    push!(ed_modules,"CountValidator")
-end
+    if (!empty)
+        ed_modules = ["SiPixelRawToClusterCUDA", "BeamSpotToPOD", "SiPixelRecHitCUDA", "CAHitNtupletCUDA", "PixelVertexProducerCUDA"]
+        es_modules = ["SiPixelFedCablingMapGPUWrapperESProducer", "SiPixelGainCalibrationForHLTGPUESProducer", "PixelCPEFastESProducer", "BeamSpotESProducer"]
+    end
+    if (validation)
+        push!(ed_modules, "CountValidator")
+    end
 
-if(histogram)
-    push!(ed_modules,"HistoValidator")
-end
-                                                                                                                                           #Not Currently Used
-##############################################################################################################################################################
-es::EventSetup = EventSetup()
-dataDir::String = (@__DIR__) * "/data/"
+    if (histogram)
+        push!(ed_modules, "HistoValidator")
+    end
+    #Not Currently Used
+    ##############################################################################################################################################################
+    es::EventSetup = EventSetup()
+    dataDir::String = (@__DIR__) * "/data/"
 
-# EP = EventProcessor(ed_modules,es_modules,dataDir)
+    # EP = EventProcessor(ed_modules,es_modules,dataDir)
 
-ev = EventProcessor(num_of_streams,ed_modules,es_modules,dataDir,validation);
-# println("Warming up")
-# @time warm_up(ev)
-# println("Warmup done")
-println("running")
-@time run_processor(ev)
+    ev = EventProcessor(num_of_streams, ed_modules, es_modules, dataDir, validation)
+    # println("Warming up")
+    # @time warm_up(ev)
+    # println("Warmup done")
+    println("running")
+    @time run_processor(ev)
 
 end
 
